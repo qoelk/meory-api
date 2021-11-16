71825a906f27022e64dd652976c97e33f40c1d1e
qoelk <qoelky@gmail.com>
2021-11-16 20:41:42 +0300

    (feat/db): some boilerplate models updates

--- app/models/assets.py
+++ app/models/assets.py
@@ -1,6 +1,6 @@
 import uuid
 
-from sqlalchemy import Column
+from sqlalchemy import Column, String
 from sqlalchemy.dialects.postgresql import UUID
 
 from app.models.model import Base
@@ -9,3 +9,6 @@ from app.models.model import Base
 class Asset(Base):
     __tablename__ = "assets"
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    thumbnail = Column(String(128))
+    full_sized = Column(String(128))
+
--- app/models/posts.py
+++ app/models/posts.py
@@ -1,6 +1,6 @@
 import uuid
 
-from sqlalchemy import Column
+from sqlalchemy import Column, String
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 
@@ -10,4 +10,6 @@ from app.models.model import Base
 class Post(Base):
     __tablename__ = "posts"
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    title = Column(String(128))
+    description = Column(String(512))
     asset = relationship("Asset")
